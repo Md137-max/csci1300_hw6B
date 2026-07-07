9a3b4aa2d6ff50bbf6fd503659caae5ecfbadba8
Maeve Dufton <Md137@icloud.com>
2026-07-07 13:24:08 -0600

    Changed Store Files

---
+++ HW6B/ScheduleAudit.h
@@ -0,0 +1,30 @@
+#ifndef STORE_H
+#define STORE_H
+
+#include "Employee.h"
+#include "Manager.h"
+#include "Cashier.h"
+
+#include <iostream>
+#include <string>
+#include<vector>
+
+using namespace std;
+
+class ScheduleAudit {
+private:
+    string plannedSchedule;
+    string actualSchedule;
+    double bestSimilarityScore;
+    int bestAlignmentIndex;
+
+public:
+    ScheduleAudit(string plannedSchedule, string actualSchedule);
+    double findBestAlignment();
+    int getBestAlignmentIndex();
+    double getBestSimilarityScore();
+    vector<string> detectDiscrepancies();
+};
+
+
+    #endif
--- HW6B/Store.cpp
+++ HW6B/Store.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 void Store::hireCashier(Cashier c) {
-    cashiers.push_back(c) 
+    cashiers.push_back(c); 
 }
 
 void Store::hireManager(Manager m) {
-    managers.push_back(m)
+    managers.push_back(m);
 }
 
 int Store::cashierCount() {
@@ -22,7 +22,15 @@ int Store::managerCount() {
 }
 
 double Store::totalPayroll() {
-    return (Cashier::calculatePay() + Manager::calculatePay());
+    double sum = 0.0;
+
+    for (int i = 0; i < cashiers.size(); i++) {
+        sum += cashiers[i].calculatePay();
+    }
+    for (int i = 0; i < managers.size(); i++) {
+        sum += managers[i].calculatePay();
+    }
+    return sum;
 }
 
 Cashier Store::topCashier() {
@@ -30,42 +38,100 @@ Cashier Store::topCashier() {
     if (cashiers.size() == 0) {
         return Cashier("None", 0.0);
     }
+    int TopCash = 0;
     for (int i = 0; i < cashiers.size(); i++) {
-        int TopCash = 0;
-        if (cashiers[i].performanceScore() > cashiers[0].performanceScore()) {
+        
+        if (cashiers[i].performanceScore() > cashiers[TopCash].performanceScore()) {
             TopCash = i;
-            return cashiers[i];
         }
-        if (i == 0) {
-            for (int j = 0; j < cashiers.size(); j++) {
-                int oldest = 0;
-                if (cashiers[i].getID() < cashiers[0].getID()) {
-                    oldest = i;
+        else if (cashiers[i].performanceScore() == cashiers[TopCash].performanceScore()) {
+           
+            if (cashiers[i].getID() < cashiers[TopCash].getID()) {
+                    TopCash = i;
                 }
-                return cashiers[i];
+    
             }
         }
-    }
+        return cashiers[TopCash];
+        }
     
-}
+     
+
 
 vector<Manager> Store::promotionEligible() {
+    vector<Manager> eligible;
     for (int i = 0; i < managers.size(); i++) {
         if (managers[i].qualifiesForPromotion() == true) {
-            promotionEligible.push_back(i);
+            eligible.push_back(managers[i]);
         }
     }
+    return eligible;
 }
 
 bool Store::promoteToManager(int employeeID, int teamSize) {
-    for (int i = 0; i < managers.size(); i++) {
-        int NewManager;
-        if (managers[i].getID() == managers[i].employeeID()) {
-            NewManager = i;
-            return managers[NewManager];
-        }
-        else {
-            return false;
+    for (int i = 0; i < cashiers.size(); i++) {
+        
+        if (cashiers[i].getID() == employeeID) {
+            string name = cashiers[i].getName();
+            double wage = cashiers[i].getHourlyWage();
+            int hours = cashiers[i].getHoursWorked();           
+           
+            Manager promoted(name, wage, teamSize); {
+                if (teamSize < 0) {
+                    teamSize = 0;
+                }
+            }
+            promoted.logHours(hours);
+            managers.push_back(promoted);
+
+            vector<Cashier> updatedCashiers;
+            for (int j = 0; j < cashiers.size(); j++) {
+                if (j != i) {
+                    updatedCashiers.push_back(cashiers[j]);
+                }
+            }
+            cashiers = updatedCashiers;
+            return true;
         }
     }
+            return false;
+        }
+    
+
+
+string Store::generateReport() {
+    string report = "";
+    report += "=== JojaMart Weekly Report ===\n";
+    report += "Total Employees: " + to_string(cashierCount() + managerCount()) + "\n";
+     string payroll = to_string(totalPayroll());
+    payroll = payroll.substr(0, payroll.find('.') + 3);
+
+    report += "Total Payroll: $" + payroll + "\n";
+
+    Cashier best = topCashier();
+
+    string score = to_string(best.performanceScore());
+    score = score.substr(0, score.find('.') + 3);
+
+    report += "Top Cashier: " + best.getName() +
+              " (" + score + " items/hr)\n";
+
+    report += "Promotion Eligible Managers: ";
+
+    vector<Manager> eligible = promotionEligible();
+
+    if (eligible.size() == 0) {
+        report += "None";
+    }
+    else {
+        for (int i = 0; i < eligible.size(); i++) {
+            report += eligible[i].getName();
+
+            if (i < eligible.size() - 1) {
+                report += ", ";
+            }
+        }
+    }
+
+    return report;
 }
